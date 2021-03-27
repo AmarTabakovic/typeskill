753a4a9865db190d2e126743afc58f09921254e4
Amar Tabakovic <tabakovicamar1@gmail.com>
2021-03-27 10:25:14 +0100

    Update texts.js

--- src/resources/texts.js
+++ src/resources/texts.js
@@ -1,5 +1,7 @@
 export const textsArray = [
     "You forget a 1000 things a day, make sure this is one of them.",
     "You can come if you want to.",
-    "We come from two different worlds but there's only one love that could make things easier.."
+    "We come from two different worlds but there's only one love that could make things easier...",
+    "This is a placeholder text. I like bananas.",
+    "The jagged face of a huge ice wall sits gloomily in the dim twilight of a Hoth day.  Luke hangs upside down, ankles frozen into icy stalactites, his extended arms within a foot of the snow floor.  One side of his face is covered in a dried mask of frozen blood.  He opens his eyes as a chilling moan of the hideous ice creature echoes off the gorge walls.  Luke pulls himself up, grabs hold of his ankles, and futilely tries to unfasten the throngs."
 ];
