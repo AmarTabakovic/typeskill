a3f77643117f75c0e0ebd36152bbd101f2790211
Amar Tabakovic <tabakovicamar1@gmail.com>
2021-10-06 06:44:47 +0200

    Adjusted texts

--- src/resources/texts.js
+++ src/resources/texts.js
@@ -1,12 +1,12 @@
 export const textsArray = [
     "You forget a thousand things every day, make sure this is one of them.",
-    "You can come if you want to.",
+    "The lead story described a do-it-yourself project called the COSMAC ELF, which consisted of a piece of perfboard full of integrated circuit chips, all wired together, plus some toggle switches and a pair of LED numeric displays",
     "We come from two different worlds but there's only one love that could make things easier...",
-    "This is a placeholder text. I like bananas.",
-    "The jagged face of a huge ice wall sits gloomily in the dim twilight of a Hoth day. Luke hangs upside down, ankles frozen into icy stalactites, his extended arms within a foot of the snow floor. One side of his face is covered in a dried mask of frozen blood. He opens his eyes as a chilling moan of the hideous ice creature echoes off the gorge walls. Luke pulls himself up, grabs hold of his ankles, and futilely tries to unfasten the throngs.",
+    "The jagged face of a huge ice wall sits gloomily in the dim twilight of a Hoth day. Luke hangs upside down, ankles frozen into icy stalactites, his extended arms within a foot of the snow floor. One side of his face is covered in a dried mask of frozen blood. He opens his eyes as a chilling moan of the hideous ice creature echoes off the gorge walls.",
     "Across the Atlantic, the picture is even more extreme. Analysis by the Economic Policy Institute, a Washington DC-based think tank, showed chief executives of the 350 largest US companies earned an average $21.3m (£16.9m) in 2019. This puts the CEO-to-worker pay ratio at 320 to 1 – more than five times the level in 1989.",
     "And evil takes a human form in Regina George. Don't be fooled, because she may seem like your typical selfish, back-stabbing, slut-faced hoe-bag. But in reality, she is so much more than that.",
     "Most computers today, in fact, process information one double word (four bytes, or 32 bits) at a time. Since 2003, PC-compatible computers have been available that process information one quad word (64 bits) at a time.",
     "One of the changes, in response to the inadequate marking of an HOV lane exit at Northside Drive in Atlanta, is the addition of different lane markings for lanes not continuing beyond an intersection or interchange to give drivers more warning that they need to switch lanes if they don’t intend to turn.",
-    "The big-Oh notation is used widely to characterize running times and space bounds in terms of some parameter n, which varies from problem to problem, but is usually defined as an intuitive notion of the 'size' of the problem."
+    "The big-Oh notation is used widely to characterize running times and space bounds in terms of some parameter n, which varies from problem to problem, but is usually defined as an intuitive notion of the 'size' of the problem.",
+    "Java uses an automatic garbage collector to manage memory in the object lifecycle. The programmer determines when objects are created, and the Java runtime is responsible for recovering the memory once objects are no longer in use."
 ];
